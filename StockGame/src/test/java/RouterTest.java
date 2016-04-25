a5ebdb6abd0a1458b8c18a7b086e1a349c8c546b
nickwp54 <nickwp54@vt.edu>
2016-04-25 19:22:26 -0400

    flushed out the RouterTest class

---
+++ StockGame/src/test/java/RouterTest.java
@@ -0,0 +1,35 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by painter on 4/25/16.
+ */
+public class RouterTest {
+
+    @Before
+    public void setup() {
+
+    }
+
+    @Test
+    public void signUpTest() throws Exception {
+
+    }
+
+    @Test
+    public void loginToAccountTest() throws Exception {
+
+    }
+
+    @Test
+    public void loginPurchaseStockTest() throws Exception {
+
+    }
+
+    @Test
+    public void loginLogoutTest() throws Exception {
+
+    }
+}
