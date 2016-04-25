f06d02c95f8129a761a8b68b4a04ecfdd9ecac7c
Matt Favero <mattfavero@gmail.com>
2016-04-25 13:52:09 -0400

    view stock works

--- StockGame/src/main/java/com/vt/stockwebgame/routes/Router.java
+++ StockGame/src/main/java/com/vt/stockwebgame/routes/Router.java
@@ -1,12 +1,26 @@
 package com.vt.stockwebgame.routes;
 
+import com.vt.stockwebgame.manager.StockManager;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static spark.Spark.get;
 
-/**
- * Created by mfav on 4/25/16.
- */
 public class Router {
     public static void main(String[] args) {
+        StockManager manager = new StockManager();
+
         get("/hello", (req, res) -> "Hello World");
+
+        get("/viewstock", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("price", manager.getStockPrice(request.queryParams("sym")));
+            model.put("data", manager.getStockData(request.queryParams("sym")));
+
+            return new ModelAndView(model, "ViewStock.html");
+        }, new VelocityTemplateEngine());
     }
 }
