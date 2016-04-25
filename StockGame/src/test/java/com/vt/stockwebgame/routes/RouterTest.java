5062d0e5abf87b2b90dec37485577677b7aa63f2
nickwp54 <nickwp54@vt.edu>
2016-04-25 19:19:01 -0400

    added router test

---
+++ StockGame/src/test/java/com/vt/stockwebgame/routes/RouterTest.java
@@ -0,0 +1,36 @@
+package com.vt.stockwebgame.routes;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by painter on 4/25/16.
+ */
+public class RouterTest {
+
+    @Test
+    public void viewStock() throws Exception {
+
+    }
+
+    @Test
+    public void buyStock() throws Exception {
+
+    }
+
+    @Test
+    public void createUserModel() throws Exception {
+
+    }
+
+    @Test
+    public void createModelForUser() throws Exception {
+
+    }
+
+    @Test
+    public void setupTestUser() throws Exception {
+
+    }
+}
