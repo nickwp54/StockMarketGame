6861a19d41e6cf73e548180e0f166d783ac4d916
nickwp54 <nickwp54@vt.edu>
2016-04-24 22:26:07 -0400

    Switched development from intellij to NetBeans

---
+++ StockWebGame/src/main/java/com/mycompany/stockwebgame/domains/User.java
@@ -0,0 +1,13 @@
+/*
+ * Created by Nicholas Phillpott on 2016.04.24  * 
+ * Copyright © 2016 Nicholas Phillpott. All rights reserved. * 
+ */
+package com.mycompany.stockwebgame.domains;
+
+/**
+ *
+ * @author painter
+ */
+public class User {
+    
+}
